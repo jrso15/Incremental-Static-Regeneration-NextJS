82c06d3b397289191f6d683e02c1890744c62f20
jrso15 <janilleolegario@gmail.com>
2021-07-08 16:59:05 +0800

    imports

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
-import Header from "../../components/header";
-import Footer from "../../components/footer";
+import Header from "../../components/HeaderLayout";
+import Footer from "../../components/FooterLayout";
 import ReactHtmlParser from "react-html-parser";
 import {
   Container,
